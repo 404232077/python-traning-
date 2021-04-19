797723d9dcc4b9b04c222d803bc5e5af0b38c464
404232077 <40423207@gm.nfu.edu.tw>
2021-04-19 23:31:53 +0800

    0419push

--- ch34/cv2.py
+++ ch34/cv2.py
@@ -1,3 +1,4 @@
+# import numpy as np
 # import cv2
 
 # cv2.namedWindow("picture1")
@@ -6,21 +7,22 @@
 # image2 = cv2.imread("crayon.png",0)
 # cv2.imshow("picture1",image1)
 # cv2.imshow("picture2",image2)
-# cv2.waitKey(3000)
+# cv2.waitKey()
 # cv2.destroyWindow("picture1")
-# cv2.waitKey(3000)
+# cv2.waitKey()
 # cv2.destroyWindow("picture2")
 
+# practice 1
 import cv2
 
 pictPath = r"C:\code\Python\Python39\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml"
 face_cascade = cv2.CascadeClassifier(pictPath)
-img = cv2.imread("spurs1.jfif")
-faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3, minSize=(20,20))
-
+img = cv2.imread("spurs3.jpeg")
+faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=8, minSize=(20,20))
+print(faces)
 cv2.rectangle(img, (img.shape[1]-140,img.shape[0]-20), (img.shape[1],img.shape[0]), (0,255,255), -1)
 
-cv2.putText(img, "Finding"+str(len(faces))+"face", (img.shape[1]-135,img.shape[0]-5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1)
+cv2.putText(img, "Finding "+str(len(faces))+" face", (img.shape[1]-135,img.shape[0]-5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1)
 
 for (x,y,w,h) in faces:
     cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
@@ -28,4 +30,3 @@ cv2.namedWindow("Face", cv2.WINDOW_NORMAL)
 cv2.imshow("Face", img)
 cv2.waitKey(0)
 cv2.destroyWindow("Face")
-cv2.imwrite("spurs2.png", img)
